61cb060cfe749e952aee5c41bff0313668667f73
--replace-all <terrencejung928@gmail.com>
2022-07-12 17:15:18 -0700

    order total price

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,8 @@ class Order < ActiveRecord::Base
     belongs_to :customer
     has_many :dishes
     has_many :menu_items, through: :dishes
+
+    def totalPrice 
+        self.menu_items.sum(:price)
+    end
 end
